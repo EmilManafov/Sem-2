079a5a84e167628a0659ccdcb12aa26e970117a3
Emil <soorpff@gmail.com>
2023-07-29 22:29:14 +0400

    task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. (Если решаете математически через рандом, то проверка не нужна на трехзначность. Если через строки, то она обязательна)
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+string str = num.ToString();
+
+if (str.Length == 3) Console.WriteLine($"Вторая цифра числа равна {str[1]}");
+else Console.WriteLine("Введено не трёхзначное число! ");
+  
